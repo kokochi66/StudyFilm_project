4b59a8567fd5a03b4ab4f989be23e8dde8231b79
kokoc <kokochikochi@gmail.com>
2021-10-22 16:14:21 +0900

    project start

---
+++ src/main/java/com/kokochi/study/FilmProject/FilmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kokochi.study.FilmProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmProjectApplication.class, args);
+	}
+
+}
