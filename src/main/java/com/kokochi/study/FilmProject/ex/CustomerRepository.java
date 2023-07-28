1898756ae947413dcab4fa0a18c510fd099ff90d
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-07-28 19:21:38 +0900

    StudyFlim으로 리파지토리 이동, 정리

--- src/main/java/com/kokochi/study/FilmProject/ex/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kokochi.study.FilmProject.ex;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-    public List<Customer> findByFirstName(String firstName);
-}
